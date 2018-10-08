a15363cac4d45f886cc133e3922b40aaac120795
Sergei Saliba <sergei.saliba@cleverbit.com>
2018-10-08 14:26:14 +0200

    Reworked individual WebAPI Get Request

--- Cleverbit.RegionsEmployeesPart2/Controllers/WebAPIController.cs
+++ Cleverbit.RegionsEmployeesPart2/Controllers/WebAPIController.cs
@@ -1,49 +1,32 @@
 ﻿using Cleverbit.RegionsEmployeesPart2.Models;
-using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 
 namespace Cleverbit.RegionsEmployeesPart2.Controllers
 {
     public class WebAPIController : ApiController
     {
-        //public IEnumerable<RegionModel> Get(int parentId)
-        //{
-        //    using(var context = new RegionsEmployeesContext())
-        //    {
-        //        //var query = context.Regions.Where(r => r.ParentRegionId == -1).AsNoTracking().OrderBy(r => r.Name).Include(e => e.RegionEmployees).ToList();
-        //        var query = context.Regions
-        //            .Where(r => r.ParentRegionId == parentId)
-        //            .OrderBy(r => r.Name)
-        //            //.ToList()
-        //            .Select(r => new RegionModel()
-        //            {
-        //                RegionId = r.RegionId,
-        //                Name = r.Name,
-        //                Employees = r.RegionEmployees
-        //                .Select(e => new EmployeeModel()
-        //                {
-        //                    Name = e.Name,
-        //                    Surname = e.Surname
-        //                })
-        //            });
-        //        return query.ToList();
-        //    }         
-        //}
-
-        public Region Get(int Id)
+        public RegionModel Get(int id)
         {
             using (var context = new RegionsEmployeesContext())
             {
-                //var query = context.Regions.Where(r => r.ParentRegionId == -1).AsNoTracking().OrderBy(r => r.Name).Include(e => e.RegionEmployees).ToList();
                 var query = context.Regions
-                    .Include(e => e.RegionEmployees)
-                    .FirstOrDefault(r => r.RegionId == Id);
-                return query;
+                    .Where(r => r.RegionId == id)
+                    .Select(r => new RegionModel()
+                    {
+                        RegionId = r.RegionId,
+                        Name = r.Name,
+                        Employees = r.RegionEmployees
+                            .Select(e => new EmployeeModel()
+                            {
+                                Name = e.Name,
+                                Surname = e.Surname
+                            })
+                    });
+
+
+                return query.FirstOrDefault();
             }
         }
 
@@ -51,11 +34,9 @@ namespace Cleverbit.RegionsEmployeesPart2.Controllers
         {
             using (var context = new RegionsEmployeesContext())
             {
-                //var query = context.Regions.Where(r => r.ParentRegionId == -1).AsNoTracking().OrderBy(r => r.Name).Include(e => e.RegionEmployees).ToList();
                 var query = context.Regions
                     .Where(r => r.ParentRegionId == -1)
                     .OrderBy(r => r.Name)
-                    //.ToList()
                     .Select(r => new RegionModel()
                     {
                         RegionId = r.RegionId,
