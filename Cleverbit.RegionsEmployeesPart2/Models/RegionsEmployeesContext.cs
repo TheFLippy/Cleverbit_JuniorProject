3625f70626bca85c3d63f21e21f8745613915288
Sergei Saliba <sergei.saliba@cleverbit.com>
2018-10-09 15:49:10 +0200

    Merge commit

--- Cleverbit.RegionsEmployeesPart2/Models/RegionsEmployeesContext.cs
+++ Cleverbit.RegionsEmployeesPart2/Models/RegionsEmployeesContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.Web;
+﻿using System.Data.Entity;
 
 namespace Cleverbit.RegionsEmployeesPart2.Models
 {
@@ -15,11 +11,5 @@ namespace Cleverbit.RegionsEmployeesPart2.Models
         {
 
         }
-
-        //
-        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        //{
-        //    base.OnModelCreating(modelBuilder);
-        //}
     }
 }
